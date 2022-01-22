f0c483dc193e776cc7a3d8aaeac2bb383d7e8948
nesmatamer <97852648+nesmatamer@users.noreply.github.com>
2022-01-22 04:45:14 -0600

    Update messages.php

--- messages.php
+++ messages.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
@@ -18,8 +18,24 @@ include "ErrorHandling.php";
 ?>
 
 <form class="form-inline" method = "POST" action = "">
-	<input type="text" name = "name" placeholder="Search" class="form-control">
+	<p>Suggestions: <span id="txtHint"></span></p>
+	<input type="text" id="txt1" onkeyup="showHint(this.value)" name = "name" placeholder="Search" class="form-control">
 	<input type="submit" value='Search' name='search' class="btn btn-default">
+	<script>
+		function showHint(str){
+			if (str.length == 0) {
+				document.getElementById("txtHint").innerHTML = "";
+				return;
+			}
+			xhttp = new XMLHttpRequest();
+			xhttp.onload = function(){
+				document.getElementById("txtHint").innerHTML = this.responseText;
+			}
+			xhttp.open("GET", "ajaxnames.php?q="+str);
+			xhttp.send();
+		}
+	</script>
+
 </form>
 <?php
 if(isset($_POST['search'])) {
