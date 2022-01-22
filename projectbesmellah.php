2d4991f487f7a10e3c145c605ddaa068489cbcc4
Marwann1 <97844476+Marwann1@users.noreply.github.com>
2022-01-22 05:11:10 +0200

    Update projectbesmellah.php

--- projectbesmellah.php
+++ projectbesmellah.php
@@ -1,44 +1,64 @@
 <html>
-<script>
-function validate(){
-var name=document.myform.fname.value;
-var password=document.myform.password.value;
-//var passw = document.getElementById('pwd').value;
-
-var regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-
-if (name==""){
-  alert("Name can't be blank");
-  return false;
-}
-else if(password=="1234"){
-{
-  alert("Password must contain character and more than 6 ");
-  return false;
-  }
-	else{
-return true;
-	}
-}
-</script>
 <head>
+<style>
+body {
+  background-image: url("electronic-devices-background-01-vector-28098779.jpg");
+}
+</style>
+
 <meta charset="utf-8">
 	<title>NYMAA</title>
 	<meta name="veiwport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script >
+function signupvalidation(){
+
+var name1 = document.getElementById("name").value;
+var name2 = document.getElementById("name2").value;
+var passw = document.getElementById("password1").value;
+var password2= document.getElementById("pwd2").value;
+
+if (name1==""){
+  alert("Name can't be blank");
+  return false;
+}
+if (name2==""){
+  alert("Name can't be blank");
+  return false;
+}
+ if(passw!==password2){
+	alert("Re-Password is not correct!")
+	return false;
+	}
+if(passw.length < 8)
+{
+  alert("Password length must be atleast 8 characters");
+  return false;
+  }
+
+
+else{
+return true;
+	}
+}
+</script>
 
 </head>
-<?php include'menu.php'; ?>
+<?php 
+	include'menu.php';
+	include'ErrorHandling.php';      
+
+	?>
 
 <div class="container">
 	<h2>Sign up</h2>
-	<form name="myform" method="post" onsubmit="return validate();" action = "databasesignup.php">
+	<form name="myform" method="post" action = "databasesignup.php">
 
 		<div class="form-group">
 			<label for="firstname">First Name:</label>
-			<input type="text" class="form-control" id="name"  placeholder="Enter First Name" name="fname" >
+			<input type="text" class="form-control" id="name"  placeholder="Enter First Name" name="fname" required >
 		</div>
 
 		<div class="form-group">
@@ -59,14 +79,14 @@ return true;
 
 	<div class="form-group">
 		<label for="pwd">Password:</label>
-		<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password" >
+		<input type="password" class="form-control" id="password1" placeholder="Enter password" name="password" required>
+	</div>
 
 		<div class="form-group">
 			<label for="pwd"> Re-Password:</label>
-			<input type="password" class="form-control" id="pwd" placeholder="Re-Enter password" name="repass" minlength="4" required>
-
-		</div>
+			<input type="password" class="form-control" id="pwd2" placeholder="Re-Enter password" name="repass" required>
 	</div>
+
 	<div class="form-group">
 		<label for="add"> Address:</label>
 		<input type="address" class="form-control" id="add" placeholder="Enter Address" name="address" required>
@@ -85,7 +105,7 @@ return true;
 		<label><input type="radio" name="gender[]">Female</label>
 	</div>
 
-	<button type="submit" class="btn btn-default">Submit</button>
+	<button type="submit" onclick="signupvalidation()" class="btn btn-default">Submit</button>
 
 	</form>
 </div>
